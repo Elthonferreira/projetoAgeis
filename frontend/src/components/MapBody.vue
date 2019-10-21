3671f18862a41a114684244b970dff6bd3dea1c6
BarbaLoira <marcoseduflautista1@gmail.com>
2019-10-20 21:53:55 -0300

    resolve front layout

--- frontend/src/components/MapBody.vue
+++ frontend/src/components/MapBody.vue
@@ -1,64 +1,85 @@
 <template>
-    <div>
-        <div :id="svgId" class="svg-container"></div>
-    </div>
+  <div class="contanier">
+    <div :id="bodyId" class="svg-container"></div>
+  </div>
 </template>
 <script>
+import BodyMap from "../assets/BodyMap";
+export default {
+  name: "BodyMap",
+  props: {
+    bodyId: ""
+  },
+  data: function() {
+    return {
+      svgId: "BodyMap",
+      mapAttr: {
+        viewBoxWidth: 1060,
+        viewBoxHeight: 800,
+        imageWidth: 1060,
+        imageHeight: 800
+      },
+      svgContainer: null
+    };
+  },
+  mounted: function() {
+    this.generateVenueMap();
+  },
+  methods: {
+    generateVenueMap: function() {
+      const vue = this;
+      const mapData = BodyMap.g.path;
+      const svgContainer = vue
+        .$svg(this.bodyId)
+        .size("100%", "100%")
+        .viewbox(
+          -320,
+          -60,
+          vue.mapAttr.viewBoxWidth,
+          vue.mapAttr.viewBoxHeight
+        );
+      vue.svgContainer = svgContainer;
+      mapData.forEach(pathObj => {
+        vue.generatePath(svgContainer, pathObj);
+      });
+    },
+    generatePath: function(svgCont, pathObj) {
+      const vue = this;
 
-    import BodyMap from "../assets/BodyMap"
-    export default {
-        name: "BodyMap",
-        data: function () {
-            return {
-                svgId: "BodyMap",
-                mapAttr: {
-                    viewBoxWidth: 1060,
-                    viewBoxHeight: 800,
-                    imageWidth: 500,
-                    imageHeight: 500,
-                },
-                svgContainer: null
-            }
-        },
-        mounted: function () {
-            this.generateVenueMap()
-        },
-        methods: {
-            generateVenueMap: function () {
-                const vue = this;
-                const mapData = BodyMap.g.path
-                const svgContainer = vue.$svg("BodyMap").size('50%', '50%').viewbox(-320, -60, vue.mapAttr.viewBoxWidth, vue.mapAttr.viewBoxHeight);
-                vue.svgContainer = svgContainer;
-                mapData.forEach((pathObj) => {
-                    vue.generatePath(svgContainer, pathObj);
-                })
-            },
-            generatePath: function (svgCont, pathObj) {
-                const vue = this;
-                
-                const attrs = {
-                    'fill': "#4676ff",
-                    'stroke': "white",
-                    'stroke-width': 2,
-                    'map-id': pathObj["-id"],
-                };
-                const element = svgCont.path(pathObj["-d"]).attr(attrs);
-                element.click(function () {
-                    if (this.node.attributes["fill"].value === '#4676ff') { // pinta a parte selecionada
-                        this.node.attributes["fill"].value = '#B63232';
-                    } else {
-                        this.node.attributes["fill"].value = '#4676ff';
-                    }
-                    
-                    const mapId = this.node.attributes["map-id"].value;
-                    vue.$emit("map-clicked", {mapId}); 
-                })
-                
-            }
+      const attrs = {
+        fill: "#4676ff",
+        stroke: "white",
+        "stroke-width": 2,
+        "map-id": pathObj["-id"]
+      };
+      const element = svgCont.path(pathObj["-d"]).attr(attrs);
+      element.click(function() {
+        if (this.node.attributes["fill"].value === "#4676ff") {
+          // pinta a parte selecionada
+          this.node.attributes["fill"].value = "#B63232";
+        } else {
+          this.node.attributes["fill"].value = "#4676ff";
         }
 
+        const mapId = this.node.attributes["map-id"].value;
+        vue.$emit("map-clicked", { mapId });
+      });
     }
+  }
+};
 </script>
 <style scoped>
-
+.contanier {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+}
+#BodyMap {
+  display: flex;
+  align-self: center;
+  flex: 1;
+}
+svg {
+  flex: 1 !important;
+}
 </style>
--- frontend/src/pages/BodyRegion1.vue
+++ frontend/src/pages/BodyRegion1.vue
@@ -1,90 +1,112 @@
 <template>
-    <div id="bodyRegion1">
-        <app-header></app-header>
-        <div class="top-info">
-          <span class="title">Selecionar Regiões</span>
-          <br>
-          <span class="subtitle">Selecione as regiões dos sintomas</span>
-        </div>
-        <map-body-front class="body-front" @map-clicked="onMapClickFront"></map-body-front>
-        <map-body-front class="body-back" @map-clicked="onMapClickBack"></map-body-front> 
-        <div class="bottom-info">
-            <span class="title-front">Frente</span>
-            <a class="waves-effect waves-light btn">Próximo</a>
-            <span class="title-back">Costas</span>
-        </div>
-        <app-footer></app-footer>
+  <div id="bodyRegion1">
+    <app-header></app-header>
+    <div class="top-info">
+      <span class="title">Selecionar Regiões</span>
+      <br />
+      <span class="subtitle">Selecione as regiões dos sintomas</span>
     </div>
+    <div class="body-area">
+      <div class="body-area-group">
+        <span>Frente</span>
+        <map-body-front class="body-front" bodyId="front" @map-clicked="onMapClickFront"></map-body-front>
+      </div>
+      <div class="body-area-group">
+        <span>Costas</span>
+        <map-body-front class="body-front" bodyId="back" @map-clicked="onMapClickFront"></map-body-front>
+      </div>
+    </div>
+    <a class="waves-effect waves-light btn">Próximo</a>
+    <div class="bottom-info"></div>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
-    import AppHeader from "../components/Header.vue"
-    import AppFooter from "../components/Footer.vue"
-    import MapBodyFront from "../components/MapBody.vue"
+import AppHeader from "../components/Header.vue";
+import AppFooter from "../components/Footer.vue";
+import MapBodyFront from "../components/MapBody.vue";
 
 export default {
-    name: "bodyRegion1",
-    components: {
-      AppHeader, AppFooter, MapBodyFront
-    },
-    data() {
-        return {
-            
-        }
-    }, 
-    methods: {
-    onMapClickFront : function(attr){
+  name: "bodyRegion1",
+  components: {
+    AppHeader,
+    AppFooter,
+    MapBodyFront
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onMapClickFront: function(attr) {
       //alert(`You clicked on state with id: ${attr.mapId} (front) `);
-    }, 
-    onMapClickBack: function(attr){
+    },
+    onMapClickBack: function(attr) {
       //alert(`You clicked on state with id: ${attr.mapId} (back) `);
     }
   }
-}
-</script>
-
+};
+</script> 
 <style scoped>
-  .title-front {
-    color: #2f52b9;
-    font-size: 26px;
-    font-weight: bold;
-    padding-left: 22%;
-  }
+.body-area {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.body-area-group {
+  flex: 1;
+  justify-content: center;
+  text-align: center;
+}
+.title-front {
+  color: #2f52b9;
+  font-size: 26px;
+  font-weight: bold;
+  padding-left: 22%;
+}
+.legend {
+  display: flex;
+  text-align: center;
+}
+.legend > span {
+  flex: 1;
+  justify-content: space-around;
+  align-self: center;
+}
 
-  .btn {
-   
-    background: #4676ff;
-  }
+.btn {
+  background: #4676ff;
+}
 
-  .title-back {
-    color: #2f52b9;
-    font-size: 26px;
-    font-weight: bold;
-    margin-right: 23.5%;
-  }
+.title-back {
+  color: #2f52b9;
+  font-size: 26px;
+  font-weight: bold;
+  margin-right: 23.5%;
+}
 
-  .bottom-info {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: -50px;
-  }
+.bottom-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: -50px;
+}
 
-  .top-info {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
+.top-info {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
 
-  .title {
-    margin-top: 20px;
-    font-size: 26px;
-    font-weight: bold;
-    color: #2f52b9;
-  }
+.title {
+  margin-top: 20px;
+  font-size: 26px;
+  font-weight: bold;
+  color: #2f52b9;
+}
 
-  .subtitle {
-    margin-top: -20px;
-    font-weight: bold;
-  }
+.subtitle {
+  margin-top: -20px;
+  font-weight: bold;
+}
 </style>
