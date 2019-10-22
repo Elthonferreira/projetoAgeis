3b04930532e31cd4379b122a28c518216f809b56
BarbaLoira <marcoseduflautista1@gmail.com>
2019-10-22 09:23:29 -0300

    card select

--- frontend/src/pages/BodyRegion1.vue
+++ frontend/src/pages/BodyRegion1.vue
@@ -8,17 +8,72 @@
     </div>
     <div class="body-area">
       <div class="body-area-group">
-        <span>Frente</span>
-        <map-body-front class="body-front" bodyId="front" @map-clicked="onMapClickFront"></map-body-front>
+        <div class="space"></div>
+        <map-body-front class="body" bodyId="front" @map-clicked="onMapClickFront"></map-body-front>
+
+        <div class="card-select">
+          <div class="input-field col s12">
+            <select v-model="areaCorporal" multiple>
+              <option value disabled>Selecione</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Membro</label>
+          </div>
+          <p>{{ areaCorporal }}</p>
+
+          <div class="input-field col s12">
+            <select v-model="sintomas" multiple>
+              <option value disabled>Selecione</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Sintomas</label>
+          </div>
+          <p>{{ sintomas}}</p>
+        </div>
+        <div class="space"></div>
       </div>
+
       <div class="body-area-group">
-        <span>Costas</span>
+        <div class="space"></div>
+        <map-body-front class="body" bodyId="back" @map-clicked="onMapClickFront"></map-body-front>
+
+        <div class="card-select">
+          <div class="input-field col s12">
+            <select v-model="areaCorporal" multiple>
+              <option value disabled>Selecione</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Membro</label>
+          </div>
+          <p>{{ areaCorporal }}</p>
+
+          <div class="input-field col s12">
+            <select v-model="sintomas" multiple>
+              <option value disabled>Selecione</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Sintomas</label>
+          </div>
+          <p>{{ sintomas}}</p>
+        </div>
+        <div class="space"></div>
+      </div>
+      <!-- <div class="body-area-group">
         <map-body-front class="body-front" bodyId="back" @map-clicked="onMapClickFront"></map-body-front>
       </div>
     </div>
     <a class="waves-effect waves-light btn">Próximo</a>
     <div class="bottom-info"></div>
-    <app-footer></app-footer>
+      <app-footer></app-footer>-->
+    </div>
   </div>
 </template>
 
@@ -35,13 +90,20 @@ export default {
     MapBodyFront
   },
   data() {
-    return {};
+    return {
+      mapFront: null,
+      mapBack: null,
+      sintomas: "",
+      areaCorporal: ""
+    };
   },
   methods: {
     onMapClickFront: function(attr) {
+      this.mapFront = attr.mapId;
       //alert(`You clicked on state with id: ${attr.mapId} (front) `);
     },
     onMapClickBack: function(attr) {
+      this.mapBack = attr.mapId;
       //alert(`You clicked on state with id: ${attr.mapId} (back) `);
     }
   }
@@ -50,30 +112,36 @@ export default {
 <style scoped>
 .body-area {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
+  flex: 1;
 }
 .body-area-group {
   flex: 1;
-  justify-content: center;
-  text-align: center;
-}
-.title-front {
-  color: #2f52b9;
-  font-size: 26px;
-  font-weight: bold;
-  padding-left: 22%;
-}
-.legend {
   display: flex;
-  text-align: center;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
-.legend > span {
-  flex: 1;
-  justify-content: space-around;
-  align-self: center;
+.body {
+  flex: 2;
+  min-width: 600px;
+  align-self: flex-end;
+  min-height: 800;
+}
+.card-select {
+  display: flex;
+  flex-direction: column;
+  min-width: 200px;
+  flex: 2;
+  background: #f7f7f7;
+  border-radius: 6px;
+  margin: 16px;
+  justify-content: space-between;
 }
 
+.space {
+  flex: 0.5;
+}
 .btn {
   background: #4676ff;
 }
