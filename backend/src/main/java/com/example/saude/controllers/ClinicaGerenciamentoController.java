48cf8f61634feef6a17c14d7ae088a3b5a804694
Elthon Ferreira <elthonferreira18@gmail.com>
2019-11-21 19:53:41 -0300

    get usuarios de pelo id da clinica (retornando null por enquanto)

--- backend/src/main/java/com/example/saude/controllers/ClinicaGerenciamentoController.java
+++ backend/src/main/java/com/example/saude/controllers/ClinicaGerenciamentoController.java
@@ -1,9 +1,8 @@
 package com.example.saude.controllers;
 
-import com.example.saude.models.Clinica;
-import com.example.saude.models.ClinicaGerenciamento;
-import com.example.saude.models.Usuario;
+import com.example.saude.models.*;
 import com.example.saude.services.ClinicaService;
+import com.example.saude.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,25 +17,58 @@ public class ClinicaGerenciamentoController {
 
     @Autowired
     private ClinicaService clinicaService;
+    private UserService userService;
 
-  /*  @GetMapping("/{usuarioId}")
-    public List<Usuario> getUsuariosByClinica (Long id_clinica){
-        Clinica clinica = new Clinica(id_clinica);
-        System.out.println(clinica.getNome());
-        return null;
-    }*/
+  // clinica - doenca_especialidade - doencas - doenca_sub_area_sintoma - sub_area_sintoma - user_sub_area_sintoma
 
-    @GetMapping("/{clinicaId}")
+    Clinica clinica = new Clinica();
+    DoencaEspecialidade doencaEspecialidade = new DoencaEspecialidade();
+    Doenca doenca = new Doenca();
+    DoencaSubAreaSintoma doencaSubAreaSintoma = new DoencaSubAreaSintoma();
+    SubAreaSintoma subAreaSintoma = new SubAreaSintoma();
+    UserSubAreaSintoma userSubAreaSintoma = new UserSubAreaSintoma();
+    Usuario usuario = new Usuario();
+    Especialidade especialidade = new Especialidade();
+
+    @GetMapping("/{clinica}")
+    public ResponseEntity<?> getUsuariosByClinica (@PathVariable("clinica") Long id_clinica){
+        //List<Usuario> usuarios = userService.getAll();
+        if (clinica.getId_clinica() == doencaEspecialidade.getClinica_id() &&
+            doenca.getId() == doencaEspecialidade.getDoenca_id() &&
+            doenca.getId() == doencaSubAreaSintoma.getDoenca_id() &&
+            doencaSubAreaSintoma.getSub_area_sintoma_id() == subAreaSintoma.getId_sub_area_sintoma() &&
+            subAreaSintoma.getId_sub_area_sintoma() == userSubAreaSintoma.getSub_area_sintoma_id() &&
+            usuario.getId() == userSubAreaSintoma.getUser_id() &&
+            clinica.getId_clinica() == id_clinica){
+
+            System.out.println(usuario.getName());
+            System.out.println(doenca.getNome());
+            System.out.println(clinica.getNome());
+            System.out.println(especialidade.getNome());
+
+            return new ResponseEntity<>(usuario, HttpStatus.OK);
+        }
+        else {
+            System.out.println(usuario.getName());
+            System.out.println(doenca.getNome());
+            System.out.println(clinica.getNome());
+            System.out.println(especialidade.getNome());
+            return new ResponseEntity<>("Clinica com id " + id_clinica+ " não existe", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+   /* @GetMapping("/{clinicaId}")
     public ResponseEntity<?> getById(@PathVariable("clinicaId") Long clinica_id) {
         Clinica clinica = clinicaService.getById(clinica_id);
         System.out.println(clinica.getNome());
         if(clinica == null) {
-            return new ResponseEntity<>("Doenca com id " + clinica_id + " não existe", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Clinica com id " + clinica_id + " não existe", HttpStatus.NOT_FOUND);
         }
         else {
             return new ResponseEntity<>(clinica, HttpStatus.OK);
         }
-    }
+    }*/
 
     @GetMapping
     public ResponseEntity<?> getAll() {
